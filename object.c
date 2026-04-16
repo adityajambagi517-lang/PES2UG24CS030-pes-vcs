43177c4bf275dc4885201552c05015c190a27dd7
adityajambagi517-lang <adityajambagi517@gmail.com>
2026-04-16 17:15:36 +0530

    Phase 1:ensure atomic object storage with rename and directory fsync

--- object.c
+++ object.c
@@ -150,6 +150,22 @@ if (write(fd, full, total_len) != (ssize_t)total_len) {
 
 fsync(fd);
 close(fd);
+
+if (rename(tmp_path, path) != 0) {
+    unlink(tmp_path);
+    free(full);
+    return -1;
+}
+
+int dir_fd = open(dir, O_RDONLY);
+if (dir_fd >= 0) {
+    fsync(dir_fd);
+    close(dir_fd);
+}
+
+free(full);
+return 0;
+
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out) {
     return -1;
 
